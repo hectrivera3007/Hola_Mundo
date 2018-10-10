ee23915841e5a37b38af1cf986562c6a14d91bdd
hectrivera3007 <43416853+hectrivera3007@users.noreply.github.com>
2018-10-10 10:02:53 -0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HolaMundo
+{
+    public partial class HolaMundo : Form
+    {
+        public HolaMundo()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola..! Soy Hector");           
+        }
+    }
+}
